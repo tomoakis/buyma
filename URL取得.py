6225362744bc44696ba8dfdddaa64c4eb08ecd6c
tomoakis <46097372+tomoakis@users.noreply.github.com>
2019-02-21 21:06:05 -0700

    Add files via upload

--- URL取得.py
+++ URL取得.py
@@ -11,26 +11,28 @@ from openpyxl.styles.fills import PatternFill
 
 
 def main():
+    spreadSheetPath = 'spreadsheets\\'
     #サイス取得開始番号の指定
-    browser = webdriver.Firefox()
+    browser = webdriver.Chrome()
     LIST = [] 
 
     #エクセル書き込み設定
     excel = input('excel名:')
-    wb = openpyxl.load_workbook('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx') 
+    wb = openpyxl.load_workbook(spreadSheetPath + excel + '.xlsx') 
     ws = wb.active
 
     NewProductNum = input('エクセル追加番号：')
+
 
     #URL取得
     if excel == 'backcountry':
         a = 'https://www.backcountry.com/new-arrivals?p=onsaleUS%3Afalse%7Ccategory%3A2.bcs.Men%27s%5C+Clothing.Men%27s%5C+Jackets&nf=1'
         b = 'https://www.backcountry.com/new-arrivals?p=onsaleUS%3Afalse%7Ccategory%3A2.bcs.Women%27s%5C+Clothing.Women%27s%5C+Jackets&nf=1'
-        urls = [b]
+        urls = [a,b]
     elif excel == 'bloomingdale':
         a = 'https://www.bloomingdales.com/shop/mens/coats-jackets?id=11548&cm_sp=NAVIGATION-_-TOP_NAV-_-11548-Clothing-Coats-%26-Jackets'
         b = 'https://www.bloomingdales.com/shop/womens-apparel/coats-jackets?id=1001520&cm_sp=NAVIGATION-_-TOP_NAV-_-1001520-Clothing-Coats-%26-Jackets'
-        urls = [b]
+        urls = [a,b]
     elif excel == 'anthropologie':
         a = 'https://www.anthropologie.com/dresses'
         b = 'https://www.anthropologie.com/jackets-coats'
@@ -40,15 +42,22 @@ def main():
         f = 'https://www.anthropologie.com/new-skirts'
         g = 'https://www.anthropologie.com/tops-blouses'
         h = 'https://www.anthropologie.com/tops-tees'
-        urls = [c,d,e,f,g,h]
+        urls = [a,c,d,e,f,g,h]
     elif excel == 'superdry':
         a = 'https://www.superdry.com/us/mens/jackets'
         b = 'https://www.superdry.com/us/womens/jackets'
         urls = [a,b]
     elif excel == 'pacsun':
-        a = 'https://www.pacsun.com/mens/jackets-coats/'
-        b = 'https://www.pacsun.com/womens/jackets-coats/'
-        urls = [b]
+        a = 'https://www.pacsun.com/mens/tees/long-sleeve-tees/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CHUF%7CGuess%7CPuma%7CRVCA%7CTommy%20Jeans%7CVans'
+        b = 'https://www.pacsun.com/mens/sweaters/'
+        c = 'https://www.pacsun.com/mens/jackets-coats/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CConverse%7CKappa%7CLevi%27s%7CRVCA%7CTimberland%7CTommy%20Jeans%7CVans'
+        d = 'https://www.pacsun.com/mens/joggers/?prefn1=brand&prefv1=adidas%7CChampion%7CConverse%7CKappa%7CRVCA'
+        e = 'https://www.pacsun.com/mens/hoodies/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CConverse%7CGuess%7CHUF%7CKappa%7Ck-Swiss%7CLevi%27s%7CLacoste%7CPuma%7CVans%7CTommy%20Jeans%7CRVCA'
+        f = 'https://www.pacsun.com/womens/graphic-t-shirts-tank-tops/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CGuess%7CLacoste%7Ck-Swiss%7CTommy%20Hilfiger%7CTommy%20Jeans'
+        g = 'https://www.pacsun.com/womens/hoodies-fleece/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CKappa%7Ck-Swiss%7CPuma%7CTommy%20Hilfiger%7CTommy%20Jeans%7CVans%7CVolcom%7CGuess%7CSuperdry'
+        h = 'https://www.pacsun.com/womens/jackets-coats/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CLevi%27s%7CRVCA%7CTommy%20Hilfiger%7CVans%7CTommy%20Jeans%7CKappa'
+        i = 'https://www.pacsun.com/womens/shoes/sneakers/?prefn1=brand&prefv1=adidas%7CConverse%7CVans%7CPuma'
+        urls = [a,b,c,d,e,f,g,h,i]
  
     for url in urls:
         browser.get(url)
@@ -58,7 +67,7 @@ def main():
         #エクセルの全URLをリストにする
         NUM = 1
         URLLIST = []
-        xl_bk = xlrd.open_workbook(excel + ".xlsx")
+        xl_bk = xlrd.open_workbook(spreadSheetPath + excel + ".xlsx")
         xl_sh = xl_bk.sheet_by_name(excel)
         while True:
             try:
@@ -82,7 +91,7 @@ def main():
                         ws['E' + str(NewProductNum)].value = href
                         category(url)
                         ws['C' + str(NewProductNum)].value = category(url)
-                        wb.save('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx')
+                        wb.save(spreadSheetPath + excel + '.xlsx')
                         NewProductNum = int(NewProductNum) + 1
                 elif excel == 'bloomingdale':
                     name = browser.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/div/div[2]/div[2]/ul/li/div/ul/li[' + str(num) + ']/div/a')
@@ -92,7 +101,7 @@ def main():
                         ws['E' + str(NewProductNum)].value = href
                         category(url)
                         ws['C' + str(NewProductNum)].value = category(url)
-                        wb.save('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx')
+                        wb.save(spreadSheetPath + excel + '.xlsx')
                         NewProductNum = int(NewProductNum) + 1
                 elif excel == 'anthropologie':
                     name = browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/div[2]/div[' + str(num) + ']/span/div[2]/a')
@@ -103,7 +112,7 @@ def main():
                         ws['E' + str(NewProductNum)].value = href
                         category(url)
                         ws['C' + str(NewProductNum)].value = category(url)
-                        wb.save('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx')
+                        wb.save(spreadSheetPath + excel + '.xlsx')
                         NewProductNum = int(NewProductNum) + 1
                 elif excel == 'superdry':
                     name = browser.find_element_by_xpath('/html/body/div[4]/div/div[1]/div[4]/div/div[' + str(num) + ']/a')
@@ -113,17 +122,17 @@ def main():
                         ws['E' + str(NewProductNum)].value = href
                         category(url)
                         ws['C' + str(NewProductNum)].value = category(url)
-                        wb.save('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx')
+                        wb.save(spreadSheetPath + excel + '.xlsx')
                         NewProductNum = int(NewProductNum) + 1
                 elif excel == 'pacsun':
-                    name = browser.find_element_by_xpath('/html/body/div[2]/div[4]/div[5]/div/ul/li[' + str(num) + ']/div/div[1]/a[1]')
+                    name = browser.find_element_by_xpath('/html/body/div[1]/div[4]/div[6]/div/ul/li[' + str(num) + ']/div/div[2]/div[1]/a')
                     href = name.get_attribute('href')
                     print(href)
                     if not href in LIST and not href in URLLIST:
                         ws['E' + str(NewProductNum)].value = href
                         category(url)
                         ws['C' + str(NewProductNum)].value = category(url)
-                        wb.save('C:\\Users\\tomoa\\Desktop\\' + excel + '.xlsx')
+                        wb.save(spreadSheetPath + excel + '.xlsx')
                         NewProductNum = int(NewProductNum) + 1
 
                 time.sleep(3)
@@ -156,7 +165,7 @@ def category(url):
     elif url == 'https://www.superdry.com/us/womens/jackets':
         return('レディース　ジャケット')
     elif url == 'https://www.pacsun.com/mens/jackets-coats/':
-        return('メンズ　ジャケット')
+        return('メンズファッション　アウター・ジャケット　ジャケットその他')
     elif url =='https://www.pacsun.com/womens/jackets-coats/':
         return('レディース　ジャケット')
     elif url =='https://www.anthropologie.com/new-sweaters?feature-product-ids=AN-4114326951628-000,AN-4114089862714-000,AN-49458623-000,AN-4114238540007-000,AN-4114089869967-000,AN-4114339214284-000,AN-4114326959962-000,AN-4113529100970-000':
@@ -171,10 +180,24 @@ def category(url):
         return('レディース　シャツ')
     elif url =='https://www.anthropologie.com/tops-tees':
         return('レディース　T')
-    
-
-
-
+    elif url =='https://www.pacsun.com/mens/tees/long-sleeve-tees/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CHUF%7CGuess%7CPuma%7CRVCA%7CTommy%20Jeans%7CVans':
+        return('メンズファッション　トップス　Tシャツ・カットソー')
+    elif url == 'https://www.pacsun.com/mens/sweaters/':
+        return('メンズファッション　トップス　ニット・セーター')
+    elif url == 'https://www.pacsun.com/mens/jackets-coats/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CConverse%7CKappa%7CLevi%27s%7CRVCA%7CTimberland%7CTommy%20Jeans%7CVans':
+        return('メンズファッション　アウター・ジャケット　ジャケットその他')
+    elif url == 'https://www.pacsun.com/mens/joggers/?prefn1=brand&prefv1=adidas%7CChampion%7CConverse%7CKappa%7CRVCA':
+        return('メンズファッション　ボトムス　パンツ')
+    elif url == 'https://www.pacsun.com/mens/hoodies/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CConverse%7CGuess%7CHUF%7CKappa%7Ck-Swiss%7CLevi%27s%7CLacoste%7CPuma%7CVans%7CTommy%20Jeans%7CRVCA':
+        return('メンズファッション　トップス　パーカー・フーディー')
+    elif url == 'https://www.pacsun.com/womens/graphic-t-shirts-tank-tops/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CGuess%7CLacoste%7Ck-Swiss%7CTommy%20Hilfiger%7CTommy%20Jeans':
+        return('レディース　トップス　Tシャツ・カットソー)
+    elif url == 'https://www.pacsun.com/womens/hoodies-fleece/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CKappa%7Ck-Swiss%7CPuma%7CTommy%20Hilfiger%7CTommy%20Jeans%7CVans%7CVolcom%7CGuess%7CSuperdry':
+        return('レディース　トップス　パーカー・フーディ')
+    elif url == 'https://www.pacsun.com/womens/jackets-coats/?prefn1=brand&prefv1=adidas%7CCalvin%20Klein%7CChampion%7CLevi%27s%7CRVCA%7CTommy%20Hilfiger%7CVans%7CTommy%20Jeans%7CKappa':
+        return('レディース　アウター　ジャケット')
+    elif url == 'https://www.pacsun.com/womens/shoes/sneakers/?prefn1=brand&prefv1=adidas%7CConverse%7CVans%7CPuma':
+        return('レディース　靴・シューズ　スニーカー')
 main()
 
 
